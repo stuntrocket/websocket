1612ac8db6f67e4ec1e9776c8e720d63d90e9f37
DannyHarding <danny@stuntrocket.co>
2021-06-05 19:03:00 +0100

    tls setting

--- config/broadcasting.php
+++ config/broadcasting.php
@@ -37,10 +37,10 @@ return [
             'app_id' => env('PUSHER_APP_ID'),
             'options' => [
                 'cluster' => env('PUSHER_APP_CLUSTER'),
-                'useTLS' => true,
-                'host' => '127.0.0.1',
-                'port' => 6001,
-                'scheme' => 'https'
+                'host'   => '127.0.0.1',
+                'port'   => 6001,
+                'scheme' => 'http',
+                'useTLS' => false, // this is no error
             ],
         ],
 
