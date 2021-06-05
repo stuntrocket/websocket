5b0b8fbca7e5810d1b00efb71dca86699ea2e46b
DannyHarding <danny@stuntrocket.co>
2021-06-05 20:23:03 +0100

    tweak settings

--- config/broadcasting.php
+++ config/broadcasting.php
@@ -36,15 +36,12 @@ return [
             'secret' => env('PUSHER_APP_SECRET'),
             'app_id' => env('PUSHER_APP_ID'),
             'options' => [
-                // 'cluster' => env('PUSHER_APP_CLUSTER'),
+                'cluster' => env('PUSHER_APP_CLUSTER'),
                 'host'   => '127.0.0.1',
                 'port'   => 6001,
                 'scheme' => 'http',
+                'encrypted' => false,
                 'useTLS' => false, // this is no error
-                'curl_options' => [
-                    CURLOPT_SSL_VERIFYHOST => 0,
-                    CURLOPT_SSL_VERIFYPEER => 0,
-                ]
             ],
         ],
 
